15958b200a0cc8e9d4e9fa7e798ac7e0413f3d39
toriumi wataru <wataru.19940213@gmail.com>
2024-05-12 21:48:49 +0900

    chore: modify dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,6 @@
-if (pr.assignee === null) {
+import { fail, danger } from "danger";
+
+if (danger.github.pr.assignee === null) {
   fail(
     "Please assign someone to merge this PR, and optionally include people who should review."
   );
